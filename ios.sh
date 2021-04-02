902c7e7c80cbdebc2de2fc3a2a17048e76c9451e
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-04-02 09:06:51 +0400

    1.1

--- ios.sh
+++ ios.sh
@@ -1,25 +1,25 @@
-MESAJ="CyberUserBot Otomatik Deploy Kuruluma Hoş Geldiniz"
-MESAJ+="\nTelegram: @TheCyberUserBot"
+MESAJ="C Y B Σ R Avtomatik Deploy Quruluma Xoş Gəldiniz!"
+MESAJ+="\nTG: @TheCyberUserBot"
 echo y | apk update
 clear
 echo -e $MESAJ
-echo "Python Yüklenir"
+echo "Python Yüklənir"
 echo y | apk add python3
 clear
 echo -e $MESAJ
-echo "Git Yüklenir"
+echo "Git Yüklənir"
 echo y | apk add git
 clear
 echo -e $MESAJ
-echo "TeleThon Yüklenir"
+echo "TeleThon Yüklənir"
 python3 -m pip install telethon
-echo "Repo klonlanıyor..."
+echo "Repo klonlanır..."
 git clone https://github.com/FaridDadashzade/Installer
 clear
 echo -e $MESAJ
 clear
 echo -e $MESAJ
-echo "Gereksinimler Yüklenir"
+echo "Bəzi fayllar yüklənir..."
 cd Installer
 python3 -m pip install wheel
 python3 -m pip install -r requirements.txt
