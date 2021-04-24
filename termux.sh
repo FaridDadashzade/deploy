6341c76a3452410c3b862be3f6d7bfef22de91c3
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-04-24 20:17:49 +0400

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,1 @@
+pkg install python wget -y && pip install telethon && wget https://raw.githubusercontent.com/FaridDadashzade/CyberV2/main/resources/startup/locals.py -O locals.py && python3 locals.py
