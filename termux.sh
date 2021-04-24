455193242d2235fd481441953248cfce4cff3878
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-04-24 20:36:52 +0400

    CyberV2 deployer

--- termux.sh
+++ termux.sh
@@ -1,1 +1,4 @@
+# CyberUserBot V2 2021
+# for https://github.com/FaridDadashzade/CyberV2
+
 pkg install python wget -y && pip install telethon && wget https://raw.githubusercontent.com/FaridDadashzade/CyberV2/main/resources/startup/locals.py -O locals.py && python3 locals.py
