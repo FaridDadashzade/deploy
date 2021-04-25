48d07c977721681dba1824577fdd97bf6221dc7a
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-04-25 08:43:15 +0400

    Create alternativ.sh

---
+++ alternativ.sh
@@ -0,0 +1,26 @@
+MESAJ="CyberUserBot Avtomatik Deploy Qurulumuna Xoş Geldiniz"
+MESAJ+="\nTG: @TheCyberUseeBot"
+apt update 
+clear
+echo -e $MESAJ
+echo "Python3 yüklenir"
+apt install python3 
+clear
+echo -e $MESAJ
+echo "Git yüklenir"
+apt install git
+clear
+echo -e $MESAJ
+echo "TeleThon yüklenir"
+pip3 install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/FaridDadashzade/Installer
+clear
+echo -e $MESAJ
+cd Installer
+clear
+echo "Bezi dosyalar yuklenir"
+echo -e $MESAJ
+pip3 install wheel
+pip3 install -r requirements.txt
+python3 -m asena_installer
