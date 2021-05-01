ff44ed2f9bff115042a29eb4eee04f460a7d1ef2
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-05-01 17:53:40 +0400

    whatscyber

---
+++ whatscyber.py
@@ -0,0 +1,8 @@
+apt update
+apt install nodejs --fix-missing
+pkg install git
+git clone https://github.com/FaridDadashzade/WhatsCyber
+cd WhatsAsenaDuplicated
+npm install @adiwajshing/baileys
+npm install chalk
+node qr.js
