76cfe45097054e89ac365206dcc409afc1d009e0
Farid <73734920+FaridDadashzade@users.noreply.github.com>
2021-05-02 13:16:32 +0400

    Create string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+MESAJ="C Y B Σ R String Alıcı"
+MESAJ+="\nTG: @TheCyberUserBot"
+pkg upgrade
+clear
+echo -e $MESAJ
+echo "Python yüklənir..."
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "TeleThon yüklənir..."
+pip install telethon
+echo "Requests/BS4 yüklənir..."
+pip install requests
+pip install bs4
+clear
+echo -e $MESAJ
+echo "Fayl yazılır..."
+curl "https://raw.githubusercontent.com/FaridDadashzade/deploy/master/cyber.py" --output "cyber.py"
+clear
+echo -e $MESAJ
+echo "Qurulum hazırdır, İndi String key-i ala bilərsiniz."
+clear
+python cyber.py
