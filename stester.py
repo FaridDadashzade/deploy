f6d231b7a24fd2688147c7c7086f9d1938325f4a
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-15 00:30:40 +0400

    hm

---
+++ stester.py
@@ -0,0 +1,7 @@
+import os, sys
+print('Packages Installing Started')
+os.system('pip install telethon==1.19.0')
+os.system('pip install requests')
+os.system('pip install colorama')
+os.system("cls" if os.name=='nt' else 'clear')
+print ('All Packages Installed Successfully')
