2b92b67f2829a674397dd37de1e970464d40e778
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-06-20 12:04:22 +0400

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -4,22 +4,16 @@ N+="\n/ /__   \  / _  / _// , _/"
 N+="\n\___/   /_/____/___/_/|_|"
 N+="\nCYBER UserBot Automatic Deploy"
 MESAJ="\nCYBER UserBot Automatic Deploy"
-BOSLUQ="\n "
 echo -e $N
-echo -e $BOSLUQ
 pkg update -y
 clear
 echo -e $N
-echo -e $BOSLUQ
 echo -e $MESAJ
-echo -e $BOSLUQ
 pkg install python -y
 pip install --upgrade pip
 clear
 echo -e $N
 echo -e $MESAJ
-echo -e $BOSLUQ
-echo -e $BOSLUQ
 pkg install git -y
 clear
 echo -e $N
